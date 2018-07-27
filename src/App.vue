1a6f96079bf8d92ef4602daeacc6229acaddf8a4
huangyunshen <yansyjing@163.com>
2018-07-27 18:04:12 +0800

    aa

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <template>
   <div id="app">
     <router-view class="app-container" name="default"></router-view>
-    <!--<div class="loading-modal">-->
-      <!--<span class="loading-img"></span>-->
-    <!--</div>-->
   </div>
 </template>
 
@@ -45,34 +42,6 @@
       box-sizing: border-box;
       height: 100%;
       overflow-y: auto;
-    }
-    .loading-modal {
-      height: 100%;
-      width: 100%;
-      position: absolute;
-      top: 0;
-      background: rgba(0,0,0,0.2);
-      .loading-img {
-        position: absolute;
-        z-index: 999999999999;
-        width: 400px;
-        height: 400px;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        background: url("./assets/images/default/loading.png") no-repeat;
-        background-size: contain;
-        animation: loading 2s linear infinite;
-      }
-    }
-  }
-
-  @keyframes loading {
-    0% {
-      transform: rotate3d(0, 0, 1, 0deg);
-    }
-    100% {
-      transform: rotate3d(0, 0, 1, 360deg);
     }
   }
 </style>
